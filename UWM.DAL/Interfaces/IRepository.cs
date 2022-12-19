86cd92a4682212cdd498680e0c9b5126b760a665
ZLUKADARK <ZLUKA@DESKTOP-Q20QITO>
2022-12-19 18:41:37 +0600

    Repository

---
+++ UWM.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace UWM.DAL.Interfaces
+{
+    public interface IRepository<Entity> where Entity : class
+    {
+        void Create(Entity item);
+        Entity Get(int id);
+        IEnumerable<Entity> GetAll();
+        void Delete(int id);
+        void Update(Entity item);
+    }
+}
